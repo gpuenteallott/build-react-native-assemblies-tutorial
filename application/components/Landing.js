21ab8af99a8ddf617c4037b2956de76663625289
Guillermo de la Puente <g.puente.allott@gmail.com>
2016-11-29 09:17:18 -0500

    Flesh out Landing screen

--- application/components/Landing.js
+++ application/components/Landing.js
@@ -1,16 +1,17 @@
-/* application/components/Landings.js */
-
+/* application/components/Landing.js */
+import Icon from 'react-native-vector-icons/Ionicons';
 import React, { Component } from 'react';
 import {
-  View,
   Text,
-  TouchableOpacity
- } from 'react-native';
+  TouchableOpacity,
+  Image,
+  View
+} from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-import NavigationBar from 'react-native-navbar';
 import Colors from '../styles/colors';
-import { globals } from '../styles';
+import { landingStyles, globals } from '../styles';
+
+const styles = landingStyles;
 
 class Landing extends Component{
   constructor(){
@@ -18,32 +19,42 @@ class Landing extends Component{
     this.visitDashboard = this.visitDashboard.bind(this);
   }
   visitDashboard(){
-    this.props.navigator.push({
-      name: 'Dashboard'
-    });
+    this.props.navigator.push({ name: 'Dashboard' })
   }
   render(){
-    let titleConfig = {
-      title: 'Landing',
-      tintColor: 'white',
-    };
     return (
-      <View style={globals.flexContainer}>
-        <NavigationBar
-          title={titleConfig}
-          tintColor={Colors.brandPrimary}
-        />
-        <View style={globals.flexCenter}>
-          <Text style={globals.h2}>
-            This is the Landing Page
-          </Text>
-          <TouchableOpacity onPress={this.visitDashboard}>
-            <Text>Go to the Dashboard</Text>
-          </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Image
+            style={styles.backgroundImage}
+            source={require('../assets/images/welcome@2x.png')}
+          />
         </View>
+        <View style={globals.flexCenter}>
+          <Image
+            style={styles.logo}
+            source={require('../assets/images/logo.png')}
+          />
+          <Text style={[globals.lightText, globals.h2, globals.mb2]}>
+            assemblies
+          </Text>
+          <Text style={[globals.lightText, globals.h4]}>
+            Where Developers Connect
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={globals.button}
+          onPress={this.visitDashboard}
+        >
+          <Icon name='ios-person' size={36} color='white' />
+          <Text style={globals.buttonText}>
+            {" "} Go to Dashboard
+          </Text>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
-}
+};
 
 export default Landing;
+
