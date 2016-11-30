eab334bd95662340112968a6db7a982e025b8a06
Guillermo de la Puente <g.puente.allott@gmail.com>
2016-11-29 22:44:58 -0500

    Create empty tabbar views

---
+++ application/shared/BackButton.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { globals } from '../styles';
+
+export default BackButton = ({ handlePress }) => (
+  <TouchableOpacity
+  	onPress={handlePress}
+  	style={globals.pa1}
+  >
+    <Icon name='ios-arrow-back' size={25} color='white' />
+  </TouchableOpacity>
+);
